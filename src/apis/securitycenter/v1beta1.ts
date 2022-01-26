0a1a42e008086b2dee3cd4195171cd4d46e13a11
Yoshi Automation <yoshi-automation@google.com>
2022-01-26 01:34:24 +0000

    fix(securitycenter): update the API
    #### securitycenter:v1beta1
    
    The following keys were changed:
    - schemas.Finding.properties.eventTime.description
    
    #### securitycenter:v1beta2
    
    The following keys were changed:
    - schemas.Finding.properties.eventTime.description
    
    #### securitycenter:v1
    
    The following keys were changed:
    - resources.folders.resources.assets.methods.updateSecurityMarks.parameters.startTime.description
    - resources.folders.resources.sources.resources.findings.methods.updateSecurityMarks.parameters.startTime.description
    - resources.organizations.resources.assets.methods.updateSecurityMarks.parameters.startTime.description
    - resources.organizations.resources.sources.resources.findings.methods.updateSecurityMarks.parameters.startTime.description
    - resources.projects.resources.assets.methods.updateSecurityMarks.parameters.startTime.description
    - resources.projects.resources.sources.resources.findings.methods.updateSecurityMarks.parameters.startTime.description
    - schemas.Finding.properties.eventTime.description

--- src/apis/securitycenter/v1beta1.ts
+++ src/apis/securitycenter/v1beta1.ts
@@ -347,7 +347,7 @@ export namespace securitycenter_v1beta1 {
      */
     createTime?: string | null;
     /**
-     * The time at which the event took place, or when an update to the finding occurred. For example, if the finding represents an open firewall it would capture the time the detector believes the firewall became open. The accuracy is determined by the detector. If the finding were to be resolved afterward, this time would reflect when the finding was resolved. Must not be set to a value greater than the current timestamp.
+     * The time the finding was first detected. If an existing finding is updated, then this is the time the update occurred. For example, if the finding represents an open firewall, this property captures the time the detector believes the firewall became open. The accuracy is determined by the detector. If the finding is later resolved, then this time reflects when the finding was resolved. This must not be set to a value greater than the current timestamp.
      */
     eventTime?: string | null;
     /**
